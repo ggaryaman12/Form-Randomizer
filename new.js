061135721e812791f2532745924610c5d6134076
Aryaman Gupta <agupta_be20@thapar.edu>
2022-05-08 19:11:51 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,6 @@
+const a= Math.floor(Math.random() * 8);
+
+if(a<2) {window.location.href ="https://vnuxtwdkc0v.typeform.com/to/UFPUGjiD";}
+else if(a<4) {window.location.href ="https://vnuxtwdkc0v.typeform.com/to/UFOIbBWp";}
+else if(a<6) {window.location.href ="https://vnuxtwdkc0v.typeform.com/to/msNjLP1i";}
+else {window.location.href = "https://www.google.com/";}
