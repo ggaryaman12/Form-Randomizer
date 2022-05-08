1d8912cc93e2de434aa36553735f0b2000ec848a
Aryaman Gupta <agupta_be20@thapar.edu>
2022-05-08 19:15:34 +0530

    Update new.js

--- new.js
+++ new.js
@@ -3,4 +3,4 @@ const a= Math.floor(Math.random() * 8);
 if(a<2) {window.location.href ="https://vnuxtwdkc0v.typeform.com/to/UFPUGjiD";}
 else if(a<4) {window.location.href ="https://vnuxtwdkc0v.typeform.com/to/UFOIbBWp";}
 else if(a<6) {window.location.href ="https://vnuxtwdkc0v.typeform.com/to/msNjLP1i";}
-else {window.location.href = "https://www.google.com/";}
+else {window.location.href = "https://l6kbxklm3it.typeform.com/to/z6m4z5Y2/";}
